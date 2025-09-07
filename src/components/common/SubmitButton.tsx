241eaf80e53adbb0096ea76bc0469e1a005bb6cd
youngrum <vinoforjob@gmail.com>
2025-09-08 05:33:36 +0900

    style - 送信ボタンンポーネント デザイン修正

--- src/components/common/SubmitButton.tsx
+++ src/components/common/SubmitButton.tsx
@@ -1,18 +1,32 @@
-'use client';
+"use client";
 
-import Button from '@mui/material/Button';
-import CircularProgress from '@mui/material/CircularProgress';
+import { Box } from "@mui/material";
+import Button from "@mui/material/Button";
+import CircularProgress from "@mui/material/CircularProgress";
 
 interface SubmitButtonProps {
-    isLoading: boolean;
-    buttonText: string;
+  isLoading: boolean;
+  buttonText: string;
 }
 
-export default function SubmitButton ({ isLoading, buttonText }: SubmitButtonProps) {
-
-    return(
-        <Button type='submit' variant='contained' disabled ={isLoading}>
-            {isLoading ? <CircularProgress size={24} color="inherit" /> : buttonText}
-        </Button>
-    );
+export default function SubmitButton({
+  isLoading,
+  buttonText,
+}: SubmitButtonProps) {
+  return (
+    <Box sx={{ display: "flex", justifyContent: "center", my: 2 }}>
+      <Button
+        type="submit"
+        variant="contained"
+        disabled={isLoading}
+        sx={{ width: "80vw" }}
+      >
+        {isLoading ? (
+          <CircularProgress size={24} color="inherit" />
+        ) : (
+          buttonText
+        )}
+      </Button>
+    </Box>
+  );
 }
