8c567216641ef12281be9e129c4f08582d18647a
youngrum <vinoforjob@gmail.com>
2025-09-12 16:39:11 +0900

    feat - loginページ作成

---
+++ src/app/login/page.tsx
@@ -0,0 +1,30 @@
+import SigninForm from "@/components/auth/SigininForm";
+import BackButton from "@/components/common/BackButton";
+import Header1 from "@/components/common/Header1";
+import SubText from "@/components/common/SubText";
+import ThemeProviderWrapper from "@/components/ThemeProviderWrapper";
+import { Stack } from "@mui/material";
+import React from "react";
+
+export default function page() {
+  const header1Text = "Welcome Back!";
+  const subText = "メールアドレスとパスワードを入力してログインしてください";
+
+  return (
+    <main>
+      <ThemeProviderWrapper>
+        <Stack
+          direction="row"
+          alignItems="center"
+          spacing={2}
+          sx={{ marginBottom: "35px" }}
+        >
+          <BackButton returnPath="/" />
+          <Header1 headerText={header1Text} />
+        </Stack>
+        <SubText subText={subText} />
+        <SigninForm />
+      </ThemeProviderWrapper>
+    </main>
+  );
+}
