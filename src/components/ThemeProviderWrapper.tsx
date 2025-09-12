8af83793c7b7ae1e6ab83f9f1057ca6fe32f4a26
youngrum <vinoforjob@gmail.com>
2025-09-12 12:35:22 +0900

    マージミス修正

--- src/components/ThemeProviderWrapper.tsx
+++ src/components/ThemeProviderWrapper.tsx
@@ -17,7 +17,7 @@ export default function ThemeProviderWrapper({
       {/* MUIのテーマプロバイダーを使用して、ThemeProviderWrapperコンポーネントの子要素にのみカスタムテーマを適用 */}
       <ThemeProvider theme={myProjectTheme}>
         <CssBaseline />
-        <Container sx={{ p: 1 }}>{children}</Container>
+        <Container maxWidth="sm"> {children}</Container>
       </ThemeProvider>
     </AppRouterCacheProvider>
   );
