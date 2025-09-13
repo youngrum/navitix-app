24864ae743b765539271d21f22c01beb1a3cb35e
youngrum <vinoforjob@gmail.com>
2025-09-13 20:41:03 +0900

    feat - profileページ作成  各フォームコンポーネント修正

--- src/components/profile/ProfileDisplay.tsx
+++ src/components/profile/ProfileDisplay.tsx
@@ -1,34 +1,34 @@
 "use client";
 
 import InputEmailArea from "@/components/common/InputEmailArea";
-import InputPasswordArea from "@/components/common/InputPasswordArea";
-import { FieldError, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
 import SubmitButton from "@/components/common/SubmitButton";
 import NoticeModal from "@/components/common/NoticeModal";
 import SignInLeads from "@/components/common/SignInLeads";
 import Divider from "@mui/material/Divider";
-import { SignUpFormValues, signUpSchema } from "@/types/form";
-import { readonly } from "zod";
+import { profileSchema, ProfileFormValues } from "@/types/form";
+import InputNameArea from "@/components/common/InputNameArea";
+import InputBirthdayArea from "@/components/common/InputBirthdayArea";
 
 export default function SigninForm() {
-  const submitText = "サインイン";
-  const leadText = "パスワードを忘れた方は";
+  const submitText = "プロフィールを修正";
+  const leadText = "パスワードを更新する方は";
   const toLogIn = "/reset-password";
-  const readOnly = false;
+  const readonly = true;
   // React Hook Formがzodスキーマ定義でバリデーションできるように宣言
   const {
     register, // TSX内でinputに渡す
     handleSubmit, // サブミットイベントのラッパー関数
     formState: { errors },
     setError, // バックエンドからのエラーメッセージを格納
-  } = useForm<SignUpFormValues>({
-    resolver: zodResolver(signUpSchema),
+  } = useForm<ProfileFormValues>({
+    resolver: zodResolver(profileSchema),
     mode: "onBlur", //フォーカスが外れた時をトリガーとする
   });
 
-  const onSubmit = (data: SignUpFormValues) => {
+  const onSubmit = (data: ProfileFormValues) => {
     console.log(data); // 検証済みのデータ
   };
 
@@ -36,16 +36,22 @@ export default function SigninForm() {
     <>
       {/** handleSubmitは第1引数に渡されたonSubmit関数を呼び出す */}
       <form onSubmit={handleSubmit(onSubmit)}>
+        <InputNameArea
+          registerProps={register}
+          errorProps={errors.accountName}
+          readonlyProps={readonly}
+        ></InputNameArea>
         <InputEmailArea
           registerProps={register}
-          errorProps={errors.email as FieldError}
-          readonlyProps={readOnly}
-        />
-        <InputPasswordArea
+          errorProps={errors.email}
+          readonlyProps={readonly}
+        ></InputEmailArea>
+        <InputBirthdayArea
           registerProps={register}
-          errorProps={errors.password as FieldError}
-          readonlyProps={readOnly}
-        />
+          errorProps={errors.birthDay}
+          readonlyProps={readonly}
+        ></InputBirthdayArea>
+
         <SignInLeads leadTextProps={leadText} toProps={toLogIn} />
         <Divider />
         <SubmitButton isLoading={false} buttonText={submitText} />
