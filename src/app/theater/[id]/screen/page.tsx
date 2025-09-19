f03f331ad723251482aade7b57a145e902ff5bd0
Your Name <you@example.com>
2025-09-19 16:10:21 +0900

    fix - パスパラメータを文字列に修正

--- src/app/theater/[id]/screen/page.tsx
+++ src/app/theater/[id]/screen/page.tsx
@@ -1,6 +1,6 @@
 import { Typography } from "@mui/material";
 
-export default async function page({params}: {params: {id: number}}){
+export default async function page({params}: {params: {id: string}}){
     const theater_id = params.id;
 
     return (
