affc932faab49244997101881c8353c8ff7496f1
youngrum <vinoforjob@gmail.com>
2025-10-19 14:07:37 +0900

    style - Grow APIで インタラクション修正

--- src/components/signup/Logo.tsx
+++ src/components/signup/Logo.tsx
@@ -1,32 +1,34 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { Box } from "@mui/material";
+import { Box, Grow } from "@mui/material";
 import Image from "next/image";
 
 export default function LogoWithFade() {
-  const [isMounted, setIsMounted] = useState(false);
+  const [inProp, setInProp] = useState(false);
 
-  // コンポーネントがクライアント側でマウントされた後に状態を変更
   useEffect(() => {
-    setIsMounted(true);
+    setTimeout(() => {
+      setInProp(true);
+    });
   }, []);
 
   return (
-    <Box
-      sx={{
-        position: "relative",
-        margin: "4rem auto 1rem",
-        width: "100%",
-        maxWidth: "10rem",
-        height: "10rem",
-        maxHeight: "10rem",
-        transition: "opacity 0.8s ease-out, transform 0.8s ease-out",
-        opacity: isMounted ? 1 : 0,
-        transform: isMounted ? "translateY(0)" : "translateY(20px)",
-      }}
-    >
-      <Image src="/logo.svg" fill alt="logoImage"></Image>
-    </Box>
+    <Grow in={inProp} timeout={1000}>
+      <Box
+        sx={{
+          position: "relative",
+          margin: "4rem auto 1rem",
+          width: "100%",
+          maxWidth: "10rem",
+          height: "10rem",
+          maxHeight: "10rem",
+          transition: inProp ? "none" : "transform 1s ease-out",
+          transform: inProp ? "translateY(0)" : "translateY(20px)",
+        }}
+      >
+        <Image src="/logo.svg" fill alt="logoImage"></Image>
+      </Box>
+    </Grow>
   );
 }
